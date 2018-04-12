0a9a9fe051d335bd9015b35a80cd4b76923f03ba
Koonail <330739264@qq.com>
2018-04-12 15:48:02 +0800

    修改测试

---
+++ src/com/wk/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.wk.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        int i = 0;
+        system.out.println(i);
+        System.out.print("hello world!");
+    }
+}
